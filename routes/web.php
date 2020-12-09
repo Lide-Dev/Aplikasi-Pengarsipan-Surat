1f8cf0f3e895eb25a07ff7847ead615e26248e2c
herlandroando <herlandrotri@gmail.com>
2020-12-09 17:34:38 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ManageMailController;
 use App\Http\Controllers\SaveMailController;
+use App\Http\Controllers\UtilityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +23,10 @@ Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('aut
 Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 Route::get('/employees', [EmployeeController::class, 'index'])->name('employee')->middleware('auth');
 Route::get('/archives/create', [SaveMailController::class, 'index'])->name('mails.create')->middleware('auth');
-Route::get('/archives/inbox', [ManageMailController::class, 'indexInbox'])->name('mails.inbox')->middleware('auth');
-Route::get('/archives/sent', [ManageMailController::class, 'indexSent'])->name('mails.sent')->middleware('auth');
+Route::get('/mails/inbox', [ManageMailController::class, 'indexInbox'])->name('mails.inbox')->middleware('auth');
+Route::get('/mails/sent', [ManageMailController::class, 'indexSent'])->name('mails.sent')->middleware('auth');
+Route::get('/errors/{error}', [UtilityController::class, 'errorHandler'])->name('error');
 
 Route::get('/login', [LoginController::class, 'show'])->name('auth');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.login');
 Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
-
